3853918c5010da8d4773786af8fc102b321b418c
workstuff <workstuffhaha@gmail.com>
2025-01-28 15:59:22 +0530

    updated and completed

--- TodoApp/obj/Debug/net8.0/TodoApp.AssemblyInfo.cs
+++ TodoApp/obj/Debug/net8.0/TodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+916dbd246dd9b39ad9a8f8cc9387e2170b2c16d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TodoApp/readme.md
@@ -0,0 +1,3 @@
+## demonstration of the ToDo application
+
+![demonstration of the to do application](image.png)
